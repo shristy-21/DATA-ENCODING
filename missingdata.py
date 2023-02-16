5e47cbf5cd917e3f68c2b6805996bda7a75ab192
shristy kumari <110484942+shristy-21@users.noreply.github.com>
2023-02-16 15:24:11 +0530

    Update missingdata.py

--- missingdata.py
+++ missingdata.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import numpy as np
 
-def fill_null_values():
+def missing_values():
    
-    df = pd.DataFrame(eval(input("Enter the DataFrame in the format: "
+    data = pd.DataFrame(eval(input("Enter the DataFrame in the format: "
                                   "pd.DataFrame({'col1': [val1, val2, ...], "
                                   "'col2': [val1, val2, ...], ...})")))
     
-    if df.empty:
-        return df
+    if data.empty:
+        return data
     
-    for column in df.columns:
-        if df[column].dtype in [np.int64, np.float64]:
-            mean = df[column].mean()
-            df[column].fillna(mean, inplace=True)
+    for column in data.columns:
+        if data[column].dtype in [np.int64, np.float64]:
+            mean = data[column].mean()
+            data[column].fillna(mean, inplace=True)
         else:
-            df[column].fillna(df[column].mode()[0], inplace=True)
+            data[column].fillna(data[column].mode()[0], inplace=True)
         
-    return df
+    return data
     
    
-df_filled = fill_null_values()
+data_filled = missing_values()
 
 
-print(df_filled)
+print(data_filled)
